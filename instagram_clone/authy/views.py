8defc6a5a6f46ae5159def33c82f7212410bf53e
logstack <jinho9613@naver.com>
2021-03-16 17:05:48 +0900

    video  11 completed

--- instagram_clone/authy/views.py
+++ instagram_clone/authy/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import update_session_auth_hash
 
 from .models import Profile
 from .forms import SignupForm, ChangePasswordForm, EditProfileForm
-#from post.models import Post, Follow, Stream
+from post.models import Post, Follow, Stream
 from django.db import transaction
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
@@ -16,162 +16,164 @@ from django.core.paginator import Paginator
 
 from django.urls import resolve
 
-# Create your views here.
+
 def UserProfile(request, username):
-	user = get_object_or_404(User, username=username)
-	profile = Profile.objects.get(user=user)
-	url_name = resolve(request.path).url_name
-	
-	if url_name == 'profile':
-		posts = Post.objects.filter(user=user).order_by('-posted')
+    user = get_object_or_404(User, username=username)
+    profile = Profile.objects.get(user=user)
+    url_name = resolve(request.path).url_name
 
-	else:
-		posts = profile.favorites.all()
+    if url_name == 'profile':
+        posts = Post.objects.filter(user=user).order_by('-posted')
 
-	#Profile info box
-	posts_count = Post.objects.filter(user=user).count()
-	following_count = Follow.objects.filter(follower=user).count()
-	followers_count = Follow.objects.filter(following=user).count()
+    else:
+        posts = profile.favorites.all()
 
-	#follow status
-	follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
+    # Profile info box
+    posts_count = Post.objects.filter(user=user).count()
+    following_count = Follow.objects.filter(follower=user).count()
+    followers_count = Follow.objects.filter(following=user).count()
 
-	#Pagination
-	paginator = Paginator(posts, 8)
-	page_number = request.GET.get('page')
-	posts_paginator = paginator.get_page(page_number)
+    # follow status
+    follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
 
-	template = loader.get_template('profile.html')
+    # Pagination
+    paginator = Paginator(posts, 8)
+    page_number = request.GET.get('page')
+    posts_paginator = paginator.get_page(page_number)
 
-	context = {
-		'posts': posts_paginator,
-		'profile':profile,
-		'following_count':following_count,
-		'followers_count':followers_count,
-		'posts_count':posts_count,
-		'follow_status':follow_status,
-		'url_name':url_name,
-	}
+    template = loader.get_template('profile.html')
 
-	return HttpResponse(template.render(context, request))
+    context = {
+        'posts': posts_paginator,
+        'profile': profile,
+        'following_count': following_count,
+        'followers_count': followers_count,
+        'posts_count': posts_count,
+        'follow_status': follow_status,
+        'url_name': url_name,
+    }
+
+    return HttpResponse(template.render(context, request))
+
 
 def UserProfileFavorites(request, username):
-	user = get_object_or_404(User, username=username)
-	profile = Profile.objects.get(user=user)
-	
-	posts = profile.favorites.all()
+    user = get_object_or_404(User, username=username)
+    profile = Profile.objects.get(user=user)
 
-	#Profile info box
-	posts_count = Post.objects.filter(user=user).count()
-	following_count = Follow.objects.filter(follower=user).count()
-	followers_count = Follow.objects.filter(following=user).count()
+    posts = profile.favorites.all()
 
-	#Pagination
-	paginator = Paginator(posts, 8)
-	page_number = request.GET.get('page')
-	posts_paginator = paginator.get_page(page_number)
+    # Profile info box
+    posts_count = Post.objects.filter(user=user).count()
+    following_count = Follow.objects.filter(follower=user).count()
+    followers_count = Follow.objects.filter(following=user).count()
 
-	template = loader.get_template('profile_favorite.html')
+    # Pagination
+    paginator = Paginator(posts, 8)
+    page_number = request.GET.get('page')
+    posts_paginator = paginator.get_page(page_number)
 
-	context = {
-		'posts': posts_paginator,
-		'profile':profile,
-		'following_count':following_count,
-		'followers_count':followers_count,
-		'posts_count':posts_count,
-	}
+    template = loader.get_template('profile_favorite.html')
 
-	return HttpResponse(template.render(context, request))
+    context = {
+        'posts': posts_paginator,
+        'profile': profile,
+        'following_count': following_count,
+        'followers_count': followers_count,
+        'posts_count': posts_count,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 
 def Signup(request):
-	if request.method == 'POST':
-		form = SignupForm(request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			email = form.cleaned_data.get('email')
-			password = form.cleaned_data.get('password')
-			User.objects.create_user(username=username, email=email, password=password)
-			return redirect('index')
-	else:
-		form = SignupForm()
-	
-	context = {
-		'form':form,
-	}
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            User.objects.create_user(username=username, email=email, password=password)
+            return redirect('index')
+    else:
+        form = SignupForm()
 
-	return render(request, 'signup.html', context)
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'signup.html', context)
 
 
 @login_required
 def PasswordChange(request):
-	user = request.user
-	if request.method == 'POST':
-		form = ChangePasswordForm(request.POST)
-		if form.is_valid():
-			new_password = form.cleaned_data.get('new_password')
-			user.set_password(new_password)
-			user.save()
-			update_session_auth_hash(request, user)
-			return redirect('change_password_done')
-	else:
-		form = ChangePasswordForm(instance=user)
+    user = request.user
+    if request.method == 'POST':
+        form = ChangePasswordForm(request.POST)
+        if form.is_valid():
+            new_password = form.cleaned_data.get('new_password')
+            user.set_password(new_password)
+            user.save()
+            update_session_auth_hash(request, user)
+            return redirect('change_password_done')
+    else:
+        form = ChangePasswordForm(instance=user)
 
-	context = {
-		'form':form,
-	}
+    context = {
+        'form': form,
+    }
 
-	return render(request, 'change_password.html', context)
+    return render(request, 'change_password.html', context)
+
 
 def PasswordChangeDone(request):
-	return render(request, 'change_password_done.html')
+    return render(request, 'change_password_done.html')
 
 
 @login_required
 def EditProfile(request):
-	user = request.user.id
-	profile = Profile.objects.get(user__id=user)
-	BASE_WIDTH = 400
+    user = request.user.id
+    profile = Profile.objects.get(user__id=user)
+    BASE_WIDTH = 400
 
-	if request.method == 'POST':
-		form = EditProfileForm(request.POST, request.FILES)
-		if form.is_valid():
-			profile.picture = form.cleaned_data.get('picture')
-			profile.first_name = form.cleaned_data.get('first_name')
-			profile.last_name = form.cleaned_data.get('last_name')
-			profile.location = form.cleaned_data.get('location')
-			profile.url = form.cleaned_data.get('url')
-			profile.profile_info = form.cleaned_data.get('profile_info')
-			profile.save()
-			return redirect('index')
-	else:
-		form = EditProfileForm()
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile.picture = form.cleaned_data.get('picture')
+            profile.first_name = form.cleaned_data.get('first_name')
+            profile.last_name = form.cleaned_data.get('last_name')
+            profile.location = form.cleaned_data.get('location')
+            profile.url = form.cleaned_data.get('url')
+            profile.profile_info = form.cleaned_data.get('profile_info')
+            profile.save()
+            return redirect('index')
+    else:
+        form = EditProfileForm()
 
-	context = {
-		'form':form,
-	}
+    context = {
+        'form': form,
+    }
 
-	return render(request, 'edit_profile.html', context)
+    return render(request, 'edit_profile.html', context)
 
 
 @login_required
 def follow(request, username, option):
-	following = get_object_or_404(User, username=username)
+    following = get_object_or_404(User, username=username)
 
-	try:
-		f, created = Follow.objects.get_or_create(follower=request.user, following=following)
+    try:
+        f, created = Follow.objects.get_or_create(follower=request.user, following=following)
 
-		if int(option) == 0:
-			f.delete()
-			Stream.objects.filter(following=following, user=request.user).all().delete()
-		else:
-			 posts = Post.objects.all().filter(user=following)[:25]
+        if int(option) == 0:
+            f.delete()
+            Stream.objects.filter(following=following, user=request.user).all().delete()
+        else:
+            posts = Post.objects.all().filter(user=following)[:25]
 
-			 with transaction.atomic():
-			 	for post in posts:
-			 		stream = Stream(post=post, user=request.user, date=post.posted, following=following)
-			 		stream.save()
+            with transaction.atomic():
+                for post in posts:
+                    stream = Stream(post=post, user=request.user, date=post.posted, following=following)
+                    stream.save()
 
-		return HttpResponseRedirect(reverse('profile', args=[username]))
-	except User.DoesNotExist:
-		return HttpResponseRedirect(reverse('profile', args=[username]))
+        return HttpResponseRedirect(reverse('profile', args=[username]))
+    except User.DoesNotExist:
+        return HttpResponseRedirect(reverse('profile', args=[username]))
--- instagram_clone/instagram_clone/urls.py
+++ instagram_clone/instagram_clone/urls.py
@@ -18,8 +18,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from authy.views import UserProfile, follow
+
 urlpatterns = [
       path('admin/', admin.site.urls),
       path('post/', include('post.urls')),
       path('user/', include('authy.urls')),
+      path('<username>/', UserProfile, name='profile'),
+      path('<username>/saved', UserProfile, name='profilefavorates'),
+      path('<username>/follow/<option>', follow, name='follow'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
